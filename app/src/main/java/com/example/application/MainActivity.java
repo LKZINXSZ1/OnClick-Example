bc7848fe742f69ceccaba7d33cdb909fa985ffd8
LKZINXS Z1 <89696884+LKZINXSZ1@users.noreply.github.com>
2023-03-11 08:01:53 -0300

    Application Files Added

---
+++ app/src/main/java/com/example/application/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.application;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+import android.graphics.*;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
